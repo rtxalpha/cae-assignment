16d2c2db37616650e6a1243aabf3410a9763be07
mt_ritik <ritik@mitiztechnologies.com>
2025-02-19 22:41:23 +0530

    migration added for the assignment

--- database/migrations/2025_02_18_215747_create_rosters_table.php
+++ database/migrations/2025_02_18_215747_create_rosters_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,9 +12,42 @@ return new class extends Migration
     {
         Schema::create('rosters', function (Blueprint $table) {
             $table->id();
-            $table->
+            $table->string('source_file');
+            $table->date('start_date');
+            $table->date('end_date');
             $table->timestamps();
         });
+
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('roster_id')->constrained();
+            $table->enum('event_type', ['DO', 'SBY', 'FLT', 'CI', 'CO', 'UNK']);
+            $table->timestamp('start_time')->nullable();
+            $table->timestamp('end_time')->nullable();
+            $table->string('location', 10)->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('flight_events', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('event_id')->constrained('events')->onDelete('cascade');
+            $table->string('flight_number', 10);
+            $table->string('departure_airport', 10);
+            $table->string('arrival_airport', 10);
+            $table->timestamp('std')->nullable();
+            $table->timestamp('sta')->nullable();
+            $table->string('aircraft_reg', 20)->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('checkin_checkout_events', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('event_id')->constrained('events')->onDelete('cascade');
+            $table->foreignId('linked_flight_id')->constrained('flight_events')->onDelete('cascade');
+            $table->string('airport_code', 10);
+            $table->timestamps();
+        });
+
     }
 
     /**
@@ -23,6 +55,9 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::dropIfExists('checkin_checkout_events');  // reverse order of creation due to foreign key constraints
+        Schema::dropIfExists('flight_events');
+        Schema::dropIfExists('events');
         Schema::dropIfExists('rosters');
     }
 };
