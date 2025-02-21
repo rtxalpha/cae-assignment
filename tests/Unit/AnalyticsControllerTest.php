bb7f2b9cd525f2c0528c98ba2dafd6577b2d2f0d
ritik <masterritikraj@gmail.com>
2025-02-21 05:46:21 +0530

    added more testing

--- tests/Unit/AnalyticsControllerTest.php
+++ tests/Unit/AnalyticsControllerTest.php
@@ -15,4 +15,26 @@ class AnalyticsControllerTest extends TestCase
                  ->assertJson(['status' => true])
                  ->assertJsonStructure(['data']);
     }
+    public function test_standy_events_from_current_date()
+    {
+        $response = $this->json('GET', '/api/events/standby', [
+            'current_date' => "2022-01-14",
+        ]);
+
+        $response->assertStatus(200)
+                 ->assertJson(['status' => true])
+                 ->assertJsonStructure(['data']);
+    }
+    public function test_events_between_from_date_to_date()
+    {
+        $response = $this->json('GET', '/api/events', [
+            'start_date' => "2022-01-10",
+            'end_date' => "2022-01-14",
+        ]);
+
+        $response->assertStatus(200)
+                 ->assertJson(['status' => true])
+                 ->assertJsonStructure(['data']);
+    }
+
 }
