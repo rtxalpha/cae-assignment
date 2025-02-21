e4218810a72d2b52ec83c98a978692bf2981d72b
ritik <masterritikraj@gmail.com>
2025-02-21 06:17:47 +0530

    unit testing code and postman collection

--- tests/Unit/AnalyticsControllerTest.php
+++ tests/Unit/AnalyticsControllerTest.php
@@ -2,28 +2,40 @@
 
 namespace Tests\Unit;
 
-use Tests\TestCase; 
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Tests\TestCase;
 class AnalyticsControllerTest extends TestCase
 {
-    public function test_next_week_flight_from_current_date()
+    public function test_flight_from_location()
     {
-        $response = $this->json('GET', '/api/flights/next-week', [
-            'current_date' => "2022-01-14",
+        $response = $this->json('GET', '/api/flights/from', [
+            'location' => "KRP",
         ]);
 
         $response->assertStatus(200)
-                 ->assertJson(['status' => true])
-                 ->assertJsonStructure(['data']);
+            ->assertJson(['status' => true])
+            ->assertJsonStructure(['data']);
+    }
+    public function test_next_week_flight_from_current_date()
+    {
+        $response = $this->json('GET', '/api/flights/next-week', [
+            'current_date' => now()->format('Y-m-d'),
+        ]);
+
+        $response->assertStatus(200)
+            ->assertJson(['status' => true])
+            ->assertJsonStructure(['data']);
     }
     public function test_standy_events_from_current_date()
     {
         $response = $this->json('GET', '/api/events/standby', [
-            'current_date' => "2022-01-14",
+            'current_date' => now()->format('Y-m-d'),
         ]);
 
         $response->assertStatus(200)
-                 ->assertJson(['status' => true])
-                 ->assertJsonStructure(['data']);
+            ->assertJson(['status' => true])
+            ->assertJsonStructure(['data']);
     }
     public function test_events_between_from_date_to_date()
     {
@@ -33,8 +45,33 @@ class AnalyticsControllerTest extends TestCase
         ]);
 
         $response->assertStatus(200)
-                 ->assertJson(['status' => true])
-                 ->assertJsonStructure(['data']);
+            ->assertJson(['status' => true])
+            ->assertJsonStructure(['data']);
     }
 
+    public function test_import_roster()
+    {
+    $filePath = storage_path('app/roster.html'); 
+
+    if (!file_exists($filePath)) {
+        $this->fail("Test file not found at: $filePath");
+    }
+
+    $file = new UploadedFile(
+        $filePath,                  
+        'roster.html',              
+        'text/html',              
+        null,                      
+        true                       
+    );
+
+    // Send the request with the actual file
+    $response = $this->postJson('/api/import-roster', [
+        'roster' => $file,
+    ]);
+
+    // Assertions
+    $response->assertStatus(200)
+             ->assertJson(['status' => true]);
+    }
 }
